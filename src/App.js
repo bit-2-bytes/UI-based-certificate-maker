8e4f39d331876259ab6e188b264852be717646df
slay-fox <ajaiss1713@gmail.com>
2022-12-25 18:40:47 +0530

    first out

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import Certificate from './Certificate';
-import Inputs from './Inputs';
-import Templates from './Templates';
+
+import { useRef, useState } from "react";
+import { useReactToPrint } from "react-to-print";
+import badge from './assets/badge.jpg'
 import './App.css';
 
 function App() {
+  const [organization, setOrgname] =useState("Academy of Learning"); 
+  const [trainingType, settrainingType] =useState("Training"); 
+  const [participantName, setparticipantName]= useState("Mr.Rajesh");
+  const [desription ,setdescription]=useState("For undergoing 30 days training on React Js")
+
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current
+  });
+
   return (
     <div>
-        <div id="app-header"> Cetificate Maker Using React js </div>
-        <div id="app-container">
-          <Templates/>
-          <Certificate/>
-          <Inputs/>
+        <div id="app-header"> Certificate Maker Using React js </div>
+        <div id="app-container">        
+        <div id="outer-border" ref={componentRef}>
+                <div id="inner-border">
+                    <img id="cert-badge" src={badge}/> 
+                    <p id="cert-orgName" className="cert-small">{organization}</p>
+                    <p className="cert-big">Certificate of {trainingType}</p>
+                    <p className="cert-small">Presented to</p>
+                    <p className="cert-big">{participantName}</p>
+                    <p className="cert-small">{desription}</p>
+                </div>
+            </div>
+            <div id="app-controls">
+            <div id="inputs">
+            <label>Name Of Issuing Organisation</label>
+            <input type="text" value={organization} onChange={(e) => setOrgname(e.target.value)}></input>
+            <label>Type of Certification </label>
+              <select value={trainingType} onChange={(e) => settrainingType(e.target.value)}>
+                  <option value="Training">Training</option>
+                  <option value="Completion">Completion</option>
+                  <option value="Participation">Participation</option>
+              </select>
+              <label>Name Of Person</label>
+            <input type="text" value={participantName} onChange={(e) => setparticipantName(e.target.value)}></input>
+            <label>Certificate desription</label>
+            <textarea type="text" value={desription} onChange={(e) => setdescription(e.target.value)}/>
+            <button onClick={handlePrint}>Download</button>
+            </div>
+        </div>
         </div>
    </div>
   );
--- src/Templates.js
+++
@@ -1,9 +0,0 @@
-const Templates=()=>{
-    return(
-        <div id="app-templates">
-            output
-        </div>
-    )
-}
-
-export default Templates;
